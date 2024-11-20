8a9373ae934b324583114b621242931a75f39d9a
Iryna Lytvyn <iryna.ohurtsova@monobank.ua>
2024-11-20 15:22:05 +0200

    - add rewrite path filter

--- websocket-server/src/main/java/org/example/websocketserver/config/WebSocketConfig.java
+++ websocket-server/src/main/java/org/example/websocketserver/config/WebSocketConfig.java
@@ -17,7 +17,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(myWebSocketHandler, "/ws")
+        registry.addHandler(myWebSocketHandler, "/ws/**")
                 .setAllowedOrigins("*");
     }
 }
